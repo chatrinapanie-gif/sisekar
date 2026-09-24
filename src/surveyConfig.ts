4426f83f18f89717a6740773065a819c5f962f98
chatrinapanie-gif <chatrinapanie@gmail.com>
2026-09-24 09:50:09 +0800

    Update surveyConfig.ts

--- src/surveyConfig.ts
+++ src/surveyConfig.ts
@@ -11,7 +11,7 @@ import { QuestionSection } from './types';
  */
 export const ADMIN_CONFIG = {
   // Masukkan URL Google Apps Script Web App Anda di sini (atau biarkan kosong untuk diisi via Portal Petugas Terproteksi):
-  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbyKYM_Es8p7iGmcqxBH11QJmaJbFr9IoZI5ht_5omH7JF_hKa_XrwjYCnrAhyQn2N9Ypg/exec', // Web App RSUD Aeramo
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbzB_2lrEYeWcfqJf55skZwYz-ls4vA57eJVf4xtWI4PWRVjJmgr0ifYgtRd2NBgQ8EnnQ/exec', // Web App RSUD Aeramo
   
   /**
    * PENGATURAN LOGO RESMI:
